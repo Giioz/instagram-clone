583901a3203a9f2296e094536416e7dc597f31c9
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-05 15:17:53 +0400

    ignore

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,5 @@
-import RegisterForm from '../../src/components/register-modal/register/RegisterForm';
+import RegisterForm from "@/src/components/register-modal/register/RegisterForm";
+
 
 export default function RegisterPage() {
   return <RegisterForm />;
