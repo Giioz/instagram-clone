abceb963e5fea045bb2342fd6c1736b3f8f67c3b
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-06 19:54:34 +0400

    feat:added login page

--- src/components/login-modal/login/LoginFormContent.tsx
+++ src/components/login-modal/login/LoginFormContent.tsx
@@ -1,5 +1,4 @@
 import TextInput from "../../register-modal/shared/TextInput";
-import Link from "next/link";
 
 interface LoginFormContentProps {
   formData: {
@@ -18,41 +17,36 @@ export default function LoginFormContent({
   isLoading,
 }: LoginFormContentProps) {
   return (
-    <form className="space-y-5 w-full" onSubmit={handleSubmit}>
-      <label className="text-[17px] font-semibold mb-1.25 block">
-        Mobile number or email
-      </label>
-      <TextInput
-        name="email"
-        type="text"
-        placeholder="Mobile number or email"
-        value={formData.email}
-        onChange={handleChange}
-        required
-      />
-      <p className="text-[15px] font-semibold mb-3.75 ">
-        You may receive notifications from us.{" "}
-        <Link href="#" className="text-blue-400 hover:underline">
-          Learn why we ask for your contact <br /> information
-        </Link>
-      </p>
+    <form
+      className="space-y-5 w-full flex flex-col items-center"
+      onSubmit={handleSubmit}
+    >
+      <div className="w-full">
+        <TextInput
+          name="email"
+          type="text"
+          placeholder="Mobile number or email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-      <label className="text-[17px] font-semibold mb-1.25 block">
-        Password
-      </label>
-      <TextInput
-        name="password"
-        type="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-        required
-      />
+      <div className="w-full">
+        <TextInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
       <button
         type="submit"
         disabled={isLoading}
-        className="w-full bg-[rgb(0,149,246)] text-white font-semibold py-3 rounded-lg hover:bg-[rgb(0,119,206)] disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
+        className="w-full bg-[rgb(0,149,246)] text-white font-semibold py-3 rounded-full hover:bg-[rgb(0,119,206)] disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
       >
         {isLoading ? "Logging in..." : "Log in"}
       </button>
