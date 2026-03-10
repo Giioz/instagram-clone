4d35128ff96ee59c3c97d01f4cc0fb2ae458c984
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-10 22:43:37 +0400

    feat:add stories to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Sidebar from "../components/layout/Sidebar";
+import Stories from "../components/stories/Stories";
+
 export default function Home() {
   return (
-    <h1>hello,world</h1>
-  )
+    <div className="flex">
+      <Sidebar />
+
+      <div className="flex-1 flex justify-center">
+        <div className="flex gap-20 bg-white mx-auto">
+          <div className="w-157.5">
+            <Stories />
+          </div>
+
+          <div className="w-79.75">{/* Right side content */}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
