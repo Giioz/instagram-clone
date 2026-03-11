2bc67104e73194c4a23a9587b207c8254e9cef6b
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-11 20:49:53 +0400

    chore: wrap RootLayout with QueryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryProvider } from "@/src/components/providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0C0F14]`}
-      >
-        {children}
-      </body>
-    </html>
+    <QueryProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0C0F14]`}
+        >
+          {children}
+        </body>
+      </html>
+    </QueryProvider>
   );
 }
