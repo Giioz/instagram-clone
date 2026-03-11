6d86870e8eee0fc862c2d35ee0bcc06faf07de2c
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-11 12:51:46 +0400

    feat: create stories circle component and import it

--- src/components/stories/StoryItem.tsx
+++ src/components/stories/StoryItem.tsx
@@ -1,5 +1,6 @@
 import type { Story, User } from "@prisma/client";
 import Image from "next/image";
+import StoryCircle from "@/src/shared/StoryCircle";
 
 interface StoryWithUser extends Story {
   user: User;
@@ -11,8 +12,8 @@ interface StoryItemProps {
 
 export default function StoryItem({ story }: StoryItemProps) {
   return (
-    <div className="flex flex-col items-center gap-2">
-      <div className="w-16 h-16 bg-linear-to-br from-purple-500 to-pink-500 rounded-full p-0.5">
+    <div className="flex flex-col items-center gap-0.5">
+      <StoryCircle>
         <div className="relative w-full h-full">
           <Image
             src={story.mediaUrl}
@@ -21,8 +22,8 @@ export default function StoryItem({ story }: StoryItemProps) {
             className="rounded-full object-cover border-2 border-black"
           />
         </div>
-      </div>
-      <span className="text-xs text-gray-300 truncate max-w-15">
+      </StoryCircle>
+      <span className="text-[12px] text-gray-300 truncate max-w-15">
         {story.user.username}
       </span>
     </div>
