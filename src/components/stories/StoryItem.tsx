16bd005a36f7491597c224c905d06ba33e84d42e
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-12 00:15:12 +0400

    ignore

--- src/components/stories/StoryItem.tsx
+++ src/components/stories/StoryItem.tsx
@@ -36,8 +36,6 @@ export default function StoryItem({ groupedStory, onClick }: StoryItemProps) {
               </div>
             )}
             {!hasMultipleStories && (
-
-              
               <Image
                 src={latestStory.mediaUrl}
                 alt={`${user.username}'s story`}
