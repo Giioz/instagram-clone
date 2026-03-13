e3e53571d70e743f9d331737e9ad2e45cd9db571
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-13 12:49:18 +0400

    refactor: remove absolute positioning from UserPreview and simplify layout

--- src/components/stories/UserPreview.tsx
+++ src/components/stories/UserPreview.tsx
@@ -18,32 +18,26 @@ export default function UserPreview({
 
   return (
     <div
-      className={`absolute ${
-        position === "left" ? "left-0" : "right-0"
-      } top-0 bottom-0 w-[50%] flex items-center justify-center opacity-90`}
+      className="relative  flex items-center justify-center w-43.25 h-77 rounded-lg overflow-hidden shadow-lg cursor-pointer hover:opacity-100 transition-all duration-300 ease-in-out transform hover:scale-105"
+      onClick={onClick}
     >
-      <div
-        className="relative w-43.25 h-77 rounded-lg overflow-hidden shadow-lg cursor-pointer hover:opacity-100 transition-all duration-300 ease-in-out transform hover:scale-105"
-        onClick={onClick}
-      >
-        <Image
-          src={stories[0].mediaUrl}
-          alt={`${user.username}'s story`}
-          fill
-          className="object-cover"
-        />
-        <div className="absolute inset-0 flex flex-col items-center justify-center">
-          <div className="w-16 h-16 rounded-full bg-[linear-gradient(45deg,#f09433_0%,#e6683c_25%,#dc2743_50%,#cc2366_75%,#bc1888_100%)] p-0.5 mb-2">
-            <div className="w-full h-full rounded-full bg-gray-900 flex items-center justify-center">
-              <span className="text-white text-lg font-semibold">
-                {user?.username?.charAt(0)?.toUpperCase() || ""}
-              </span>
-            </div>
+      <Image
+        src={stories[0].mediaUrl}
+        alt={`${user.username}'s story`}
+        fill
+        className="object-cover"
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center">
+        <div className="w-16 h-16 rounded-full bg-[linear-gradient(45deg,#f09433_0%,#e6683c_25%,#dc2743_50%,#cc2366_75%,#bc1888_100%)] p-0.5 mb-2">
+          <div className="w-full h-full rounded-full bg-gray-900 flex items-center justify-center">
+            <span className="text-white text-lg font-semibold">
+              {user?.username?.charAt(0)?.toUpperCase() || ""}
+            </span>
           </div>
-          <p className="text-white text-base text-center font-medium">
-            {user?.username}
-          </p>
         </div>
+        <p className="text-white text-base text-center font-medium">
+          {user?.username}
+        </p>
       </div>
     </div>
   );
