82c8fd4e7fc0e33fea1297e95e545d517ff470a9
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-17 02:15:36 +0400

    feat: add UserProfile and API response types

---
+++ src/modules/user-profile/index.ts
@@ -0,0 +1,6 @@
+export { getUserProfile } from './services/getUserProfile';
+export * from './types';
+export { GET as getUserProfileRoute } from './server/routes/get.route';
+export { default as ProfileHeader } from './components/section/ProfileHeader';
+export { default as ProfilePosts } from './components/section/ProfilePosts';
+export { default as ProfilePage } from './components/section/page';
