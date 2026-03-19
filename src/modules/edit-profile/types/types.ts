0f40771c62089b750e0afa9c2346d21a945bc4c5
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-19 23:15:34 +0400

    feat:add edit-profile types

---
+++ src/modules/edit-profile/types/types.ts
@@ -0,0 +1,14 @@
+export type Gender = "MALE" | "FEMALE" | "CUSTOM" | "PREFER_NOT_TO_SAY";
+
+export interface UserProfile {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+  bio: string | null;
+  website: string | null;
+  gender: Gender;
+  imageUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
