d1aa1199b3a351f88217d5607d01f2c4985ebe04
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-19 23:07:03 +0400

    refactor: reorganize folder structure and move files

--- src/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import LoginForm from "@/src/modules/auth/login/component/sections/LoginForm";
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
--- src/app/register/page.tsx
+++
@@ -1,6 +0,0 @@
-import RegisterForm from "@/src/modules/auth/register/components/common/RegisterForm";
-
-
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
