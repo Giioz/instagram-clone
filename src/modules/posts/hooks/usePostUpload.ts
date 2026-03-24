394b9818978ef8436ddc07c4b09cf6ddeafc9887
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-24 13:23:28 +0400

    feat(posts): add usePostUpload hook

---
+++ src/modules/posts/hooks/usePostUpload.ts
@@ -0,0 +1,16 @@
+'use client';
+
+import { useState } from 'react';
+import React from 'react';
+import { PostUploader } from '../components/PostUploader';
+
+export function usePostUpload(onUpload: (mediaUrl: string) => Promise<void>) {
+  const [isUploading, setIsUploading] = useState(false);
+
+  const PostUploaderComponent = () => React.createElement(PostUploader, { onUpload });
+
+  return {
+    isUploading,
+    PostUploader: PostUploaderComponent
+  };
+}
