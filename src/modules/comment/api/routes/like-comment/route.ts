4706b1c105f88394d71a9eede0cca09948a28188
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-25 20:31:52 +0400

    fix:fixed like-comment routes.ts imports

--- src/modules/comment/api/routes/like-comment/route.ts
+++ src/modules/comment/api/routes/like-comment/route.ts
@@ -1,6 +1,6 @@
 import { GET } from "./get.route";
 import { POST } from "./post.route";
-import { DELETE } from "./delete.route";
-import { POST as LIKE } from "./like.route";
+import { DELETE } from "../comment/delete.route";
+import { DELETE as LIKE_DELETE } from "./like.route";
 
-export { GET, POST, DELETE, LIKE };
+export { GET, POST, DELETE, LIKE_DELETE };
