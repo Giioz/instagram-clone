6cba100136c96ebc02b0bebd0f27acd8de732d70
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-25 20:56:48 +0400

    refactor: move comment routes from api to server directory

--- src/modules/comment/api/routes/like-comment/route.ts
+++
@@ -1,6 +0,0 @@
-import { GET } from "./get.route";
-import { POST } from "./post.route";
-import { DELETE } from "../comment/delete.route";
-import { DELETE as LIKE_DELETE } from "./like.route";
-
-export { GET, POST, DELETE, LIKE_DELETE };
