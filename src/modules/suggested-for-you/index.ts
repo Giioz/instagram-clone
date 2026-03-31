53a736e2cd146c582a56c2a8917520fd637962aa
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-03-31 13:22:09 +0400

    feat(suggested-for-you): add index and types for SuggestedUser and MutualPreviewUser

---
+++ src/modules/suggested-for-you/index.ts
@@ -0,0 +1,3 @@
+export { SuggestedForYou } from './components/section/SuggestedForYou';
+export { SuggestedUserCard } from './components/common/SuggestedUserCard';
+export { SuggestedForYouSkeleton } from './components/skeletons/SuggestedForYouSkeleton';
